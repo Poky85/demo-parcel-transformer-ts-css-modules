5ca24a1cc9f4af6c706dadea9580fbc4824211b7
Jiří Pokorný <poky85@gmail.com>
2022-02-26 16:49:59 +0100

    added example of using parcel-transformer-ts-css-modules with another transformers

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as styles from "./styles.less";
+
+document.getElementById("app")!.innerHTML = `
+  <p class="${styles.foo}">Hello world</p>
+`;
---
+++ src/styles.less.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+  readonly "bar": string;
+  readonly "foo": string;
+};
+export = styles;
+
